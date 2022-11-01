a5e7ff875ae66b1993ce1559a21fb8141c727f93
LynnYue10 <116870883+LynnYue10@users.noreply.github.com>
2022-11-01 18:30:43 +0800

    ylupdate1

--- AnswerEntityMapper.java
+++ AnswerEntityMapper.java
@@ -1,13 +1,17 @@
 package com.aim.questionnaire.dao;
 
-import com.aim.questionnaire.dao.entity.AnswerEntity;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import com.aim.questionnaire.dao.entity.AnswerEntity;
+import com.aim.questionnaire.dao.entity.ProjectEntity;
+
 @Repository
 public interface AnswerEntityMapper {
+
 	
     /**
      * 查询用户列表（模糊搜索）
@@ -22,5 +26,79 @@ public interface AnswerEntityMapper {
      * @return
      */
     List<Map<String,Object>> queryQuestionnaireByProjectId( Map<String, Object> map);
+	
+	/**
+     * 查询答者列表
+     * @param String
+     * @return 
+     */
+	@Mapper
+	List<Map<String, Object>> queryAnswerList(String username,String answername);
+	
+	/**
+     * 根据答者id删除
+     * @param id
+     * @return
+     */
+	@Mapper
+    void deleteAnswerById(String id);
+
+    /**
+     * 添加答者
+     * @param answerEntity
+     * @return
+     */
+	@Mapper
+    void addAnswer(AnswerEntity answerEntity);
+
+	
+	/**
+     * 查询答者
+     * @param answerEntity
+     * @return
+     */
+	@Mapper
+	List<Map<String, Object>> ifhasanswer(String answername);
+
+	
+	/**
+     * 查询答者
+     * @param id
+     * @return
+     */
+	@Mapper
+	AnswerEntity findAnswerById(String id);
+
+	/**
+     * 修改答者
+     * @param id
+     * @return
+     */
+	@Mapper
+	void modifyAnswer(AnswerEntity answerEntity);
+	
+	/**
+     * 查询delete答者列表
+     * @param String
+     * @return 
+     */
+	@Mapper
+	List<Map<String, Object>> queryDeletedAnswerList(String username,String answername);
+	
+	/**
+     * 根据答者id彻底删除
+     * @param id
+     * @return
+     */
+	@Mapper
+    void finnaldeleteAnswerById(String id);
+	
+	/**
+     * 根据答者id恢复
+     * @param id
+     * @return
+     */
+	@Mapper
+    void findbackAnswerById(String id);
 
 }
