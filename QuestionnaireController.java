089eeb7e51ae0961c945d773556c4d48ee4e0781
ScTiVCrMn287 <108472397+ScTiVCrMn287@users.noreply.github.com>
2022-11-01 20:01:54 +0800

    wxyUpdate1.3

--- QuestionnaireController.java
+++ QuestionnaireController.java
@@ -93,6 +93,79 @@ public class QuestionnaireController {
         return httpResponseEntity;
     }
     
+    
+    /**
+     * 查询全部相同类型的问卷
+     * @param questionnaireEntity
+     * @return
+     */
+    @RequestMapping(value = "/queryHistoryQuestionnaireInfo", method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity queryHistoryQuestionnaire(@RequestBody Questionnaire questionnaire) {
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+        try {
+            List<Map<String, Object>> list = questionnaireService.queryAllQuestionnaireInfo();
+            List<Map<String, Object>> history = new ArrayList<Map<String,Object>>();
+            for(Map<String, Object> m:list) {
+            	if(!m.get("groupname").equals("模板")) {
+            		if(m.get("dataId").equals(questionnaire.getDataId())) {
+            			history.add(m);
+            		}
+            	}
+            }
+            httpResponseEntity.setCode(Constans.SUCCESS_CODE);
+            httpResponseEntity.setMessage(Constans.STATUS_MESSAGE);
+            httpResponseEntity.setData(history);
+        } catch (Exception e) {
+            logger.error("delete questionnaire error", e);
+            httpResponseEntity.setCode(Constans.EXIST_CODE);
+            httpResponseEntity.setMessage(Constans.EXIST_MESSAGE);
+        }
+        return httpResponseEntity;
+    }
+    
+    /**
+     * 查询全部问卷
+     * @param questionnaireEntity
+     * @return
+     */
+    @RequestMapping(value = "/showAllQuestionnaireInfo", method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity showAllQuestionnaireInfo(@RequestBody Map<String, Object> map) {
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+        try {
+            PageInfo list = questionnaireService.showAllQuestionnaireInfo(map);
+            httpResponseEntity.setCode(Constans.SUCCESS_CODE);
+            httpResponseEntity.setMessage(Constans.STATUS_MESSAGE);
+            httpResponseEntity.setData(list);
+        } catch (Exception e) {
+            logger.error("delete questionnaire error", e);
+            httpResponseEntity.setCode(Constans.EXIST_CODE);
+            httpResponseEntity.setMessage(Constans.EXIST_MESSAGE);
+        }
+        return httpResponseEntity;
+    }
+    
+    /**
+     * 查询全部已删除问卷
+     * @param questionnaireEntity
+     * @return
+     */
+    @RequestMapping(value = "/showAllDeletedQuestionnaireInfo", method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity showAllDeletedQuestionnaireInfo(@RequestBody Map<String, Object> map) {
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+        try {
+            PageInfo list = questionnaireService.showAllDeletedQuestionnaireInfo(map);
+            httpResponseEntity.setCode(Constans.SUCCESS_CODE);
+            httpResponseEntity.setMessage(Constans.STATUS_MESSAGE);
+            httpResponseEntity.setData(list);
+        } catch (Exception e) {
+            logger.error("delete questionnaire error", e);
+            httpResponseEntity.setCode(Constans.EXIST_CODE);
+            httpResponseEntity.setMessage(Constans.EXIST_MESSAGE);
+        }
+        return httpResponseEntity;
+    }
+    
+    
     /**
      * 设计问卷相关
      * @param questionnaire
@@ -104,10 +177,12 @@ public class QuestionnaireController {
          try {
              questionnaireService.modifyQuestionnaire(questionnaire);
              httpResponseEntity.setCode(Constans.SUCCESS_CODE);
+             httpResponseEntity.setData(questionnaire.getId());
              httpResponseEntity.setMessage(Constans.UPDATE_MESSAGE);
          } catch (Exception e) {
              logger.error("modify questionnaire error", e);
              httpResponseEntity.setCode(Constans.EXIST_CODE);
+             httpResponseEntity.setData(questionnaire.getId());
              httpResponseEntity.setMessage(Constans.EXIST_MESSAGE);
          }
          return httpResponseEntity;
@@ -123,16 +198,30 @@ public class QuestionnaireController {
         HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
         try {
             Map<String, Object> map = questionnaireService.queryQuestionnaireById(questionnaire.getId());
-            String status = String.valueOf(map.get("status"));
-            if(!status.equals("2")) {
             	httpResponseEntity.setCode(Constans.SUCCESS_CODE);
                 httpResponseEntity.setMessage(Constans.STATUS_MESSAGE);
                 httpResponseEntity.setData(map);
-            }else {
-            	httpResponseEntity.setCode(Constans.EXIST_CODE);
-                httpResponseEntity.setMessage(Constans.COPY_EXIT_UPDATE_MESSAGE);
-            }
-
+        } catch (Exception e) {
+            logger.error("query questionnaire error", e);
+            httpResponseEntity.setCode(Constans.EXIST_CODE);
+            httpResponseEntity.setMessage(Constans.EXIST_MESSAGE);
+        }
+        return httpResponseEntity;
+    }
+    
+    /**
+     * 根据id查找删除的问卷
+     * @param questionnaire
+     * @return
+     */
+    @RequestMapping(value = "/queryDeletedQuestionnaireById", method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity queryDeletedQuestionnaireById(@RequestBody Questionnaire questionnaire) {
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+        try {
+            Map<String, Object> map = questionnaireService.queryDeletedQuestionnaireById(questionnaire.getId());
+            httpResponseEntity.setCode(Constans.SUCCESS_CODE);
+                httpResponseEntity.setMessage(Constans.STATUS_MESSAGE);
+                httpResponseEntity.setData(map);
         } catch (Exception e) {
             logger.error("query questionnaire error", e);
             httpResponseEntity.setCode(Constans.EXIST_CODE);
@@ -156,8 +245,6 @@ public class QuestionnaireController {
             httpResponseEntity.setMessage(Constans.STOP_EXIT_UPDATE_MESSAGE);
            }else if(questionnaire.getStatus().equals("2")){
             Date releasetime = new Date();
-            //SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            //System.out.println(sdf.format(releasetime));
             System.out.println(releasetime);
             questionnaire.setReleaseTime(releasetime);
             questionnaireService.modifyQuestionnaireInfo(questionnaire);
@@ -172,40 +259,81 @@ public class QuestionnaireController {
         return httpResponseEntity;
     }
     
+    /**
+     * 修改问卷信息
+     * @param questionnaire
+     * @return
+     */
+    @RequestMapping(value = "/ModifyQuestionnaireIsDelete", method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity ModifyQuestionnaireIsDelete(@RequestBody Questionnaire questionnaire) {
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+        if(questionnaire.getGroupname().equals("未发送")||questionnaire.getGroupname().equals("模板")) {
+        	questionnaireService.modifyQuestionnaireInfo(questionnaire);
+        	httpResponseEntity.setCode(Constans.SUCCESS_CODE);
+            httpResponseEntity.setMessage(Constans.DELETE_MESSAGE);
+        }else {
+          httpResponseEntity.setCode(Constans.DELETE_FAILend_CODE);
+          httpResponseEntity.setMessage(Constans.DELETE_FAILend);	
+        }
+        
+        return httpResponseEntity;
+    }
+    
+    
+    /**
+     * 修改问卷信息
+     * @param questionnaire
+     * @return
+     */
+    @RequestMapping(value = "/RecoverQuestionnaire", method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity RecoverQuestionnaire(@RequestBody Questionnaire questionnaire) {
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+        if(questionnaire.getGroupname().equals("未发送")||questionnaire.getGroupname().equals("模板")) {
+        	questionnaireService.modifyQuestionnaireInfo(questionnaire);
+        	httpResponseEntity.setCode(Constans.SUCCESS_CODE);
+            httpResponseEntity.setMessage(Constans.RECOVER_MESSAGE);
+        }else {
+          httpResponseEntity.setCode(Constans.DELETE_FAILend_CODE);
+          httpResponseEntity.setMessage(Constans.RECOVER_FAILend);	
+        }
+        
+        return httpResponseEntity;
+    }
+    
     @RequestMapping(value = "/deleteQuestionnaireInfo",method = RequestMethod.POST, headers = "Accept=application/json")
     public HttpResponseEntity deleteProjectById(@RequestBody Questionnaire questionnaire) throws ParseException{
      HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
         int result = questionnaireService.deleteQuestionnaireById(questionnaire.getId());
-        
-        if(result == 1) {
-            httpResponseEntity.setCode(Constans.QUESTIONNAIRE_SEND_CODE);
-            httpResponseEntity.setData(questionnaire.getId());
-            httpResponseEntity.setMessage(Constans.MODEL_DELETE_FAILend);
+        if(result==1) {
+          httpResponseEntity.setCode(Constans.EXIST_CODE);
+          httpResponseEntity.setData(questionnaire.getId());
+          httpResponseEntity.setMessage(Constans.DELETE_FAILend);
         }else {
-         if(result == 2) {
-                httpResponseEntity.setCode(Constans.QUESTIONNAIRE_SEND_CODE);
-                httpResponseEntity.setData(questionnaire.getId());
-                httpResponseEntity.setMessage(Constans.MODEL_DELETE_FAILsend);
-            }
-         else {
-          if(result == 3) {
-           httpResponseEntity.setCode(Constans.SUCCESS_CODE);
-                    httpResponseEntity.setData(questionnaire.getId());
-                    httpResponseEntity.setMessage(Constans.MODEL_DELETE_FAILstart);
-                }
-          else {
-           httpResponseEntity.setCode(Constans.SUCCESS_CODE);
-                    httpResponseEntity.setData(questionnaire.getId());
-                    httpResponseEntity.setMessage(Constans.DELETE_MESSAGE);
-                    
-          }
-           
-         }
-        } 
-         
+        	httpResponseEntity.setCode(Constans.SUCCESS_CODE);
+        	httpResponseEntity.setData(questionnaire.getId());
+        	httpResponseEntity.setMessage(Constans.DELETE_MESSAGE); 
+        }
        
         return httpResponseEntity;
     }  
+    
+    @RequestMapping(value = "/deleteQuestionnaireInfoCompletely",method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity deleteProjectByIdCompletely(@RequestBody Questionnaire questionnaire) throws ParseException{
+     HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+        int result = questionnaireService.deleteQuestionnaireByIdCompletely(questionnaire.getId());
+        if(result==0) {
+        	httpResponseEntity.setCode(Constans.SUCCESS_CODE);
+        	httpResponseEntity.setData(questionnaire.getId());
+        	httpResponseEntity.setMessage(Constans.DELETE_MESSAGE);
+        }else {
+        	httpResponseEntity.setCode(Constans.EXIST_CODE);
+            httpResponseEntity.setData(questionnaire.getId());
+            httpResponseEntity.setMessage(Constans.DELETE_COM_FAILend);
+        }
+       
+        return httpResponseEntity;
+    }  
+
     
     
     /**
@@ -220,7 +348,7 @@ public class QuestionnaireController {
             List<Map<String, Object>> list = questionnaireService.queryAllQuestionnaireInfo();
             List<Map<String, Object>> model = new ArrayList<Map<String,Object>>();
             for(Map<String, Object> m :list) {
-             if(m.get("projectId")==null) {
+             if(m.get("groupname").equals("模板")) {
               model.add(m);
              }
             }
