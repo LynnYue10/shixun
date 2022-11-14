45ab640f5294846bc04facab624b055d5b9389da
ScTiVCrMn287 <108472397+ScTiVCrMn287@users.noreply.github.com>
2022-11-14 22:26:29 +0800

    wxy10

--- QuestionnaireService.java
+++ QuestionnaireService.java
@@ -57,6 +57,7 @@ public class QuestionnaireService {
 	public int addQuestionnaire(Questionnaire questionnaire) {
 		questionnaire.setId(UUIDUtil.getOneUUID());
 		if (questionnaire.getStartTime() != null && questionnaire.getEndTime() != null) {
+			// 设置开始或结束时间
 			Long startTimeL = questionnaire.getStartTime().getTime();
 			Long endTimeStL = questionnaire.getEndTime().getTime();
 			String startTimeStr = String.valueOf(startTimeL);
@@ -65,8 +66,10 @@ public class QuestionnaireService {
 			Date endTime = DateUtil.getMyTime(endTimeStr);
 			questionnaire.setStartTime(startTime);
 			questionnaire.setEndTime(endTime);
+			// 设置群组以及未被删除
 			questionnaire.setIsdelete("0");
 			questionnaire.setGroupname("未发送");
+			// 设施status
 			Date date = new Date();
 			long now = date.getTime();
 			if (now < startTimeL) {
@@ -74,9 +77,29 @@ public class QuestionnaireService {
 			} else {
 				questionnaire.setStatus("1");
 			}
+			// 获取模板的题目和选型并赋予给这个新的问卷
+			if (questionnaire.getProjectId() != null) {
+				Map<String, Object> target = questionnaireService.queryQuestionnaireById(questionnaire.getProjectId());
+				String question = (String) target.get("question");
+				String questionTitle = (String) target.get("questionTitle");
+				
+				questionnaire.setQuestion(question);
+				questionnaire.setQuestionTitle(questionTitle);
+			
+			}
+
 			questionnaireMapper.addQuestionnaire(questionnaire);
 		} else {
 			questionnaire.setStatus("0");
+			// 获取模板的题目和选型并赋予给这个新的问卷
+			if (questionnaire.getProjectId() != null) {
+				Map<String, Object> target = questionnaireService.queryQuestionnaireById(questionnaire.getProjectId());
+				String question = (String) target.get("question");
+				String questionTitle = (String) target.get("questionTitle");
+				 questionnaire.setQuestion(question);
+				 questionnaire.setQuestionTitle(questionTitle);
+				 
+			}
 			questionnaireMapper.addQuestionnaire(questionnaire);
 		}
 		return 0;
@@ -99,7 +122,7 @@ public class QuestionnaireService {
 	public List<Map<String, Object>> queryAllQuestionnaireInfo() {
 		return questionnaireMapper.selectAllQuestionnaire();
 	}
-	
+
 	/**
 	 * 查询全部问卷
 	 * 
@@ -118,8 +141,7 @@ public class QuestionnaireService {
 	public Map<String, Object> queryQuestionnaireById(String questionnaireId) {
 		return questionnaireMapper.selectQuestionnaireById(questionnaireId);
 	}
-	
-	
+
 	/**
 	 * 根据问卷id查询删除的问卷信息
 	 * 
@@ -138,19 +160,19 @@ public class QuestionnaireService {
 	public PageInfo showAllQuestionnaireInfo(Map<String, Object> map) {
 		int pagenum = (int) map.get("pageNum");
 		int pageSize = (int) map.get("pageSize");
-		String questionnaireName = null ;
-		if(map.get("questionnaireName")==null) {
-			questionnaireName="";
-		}else if(map.get("questionnaireName")!=null) {
-			questionnaireName=(String)map.get("questionnaireName");
+		String questionnaireName = null;
+		if (map.get("questionnaireName") == null) {
+			questionnaireName = "";
+		} else if (map.get("questionnaireName") != null) {
+			questionnaireName = (String) map.get("questionnaireName");
 		}
 		PageHelper.startPage(pagenum, pageSize);
-		List<Map<String,Object>> resultList = new ArrayList<Map<String,Object>>();
-		if(questionnaireName.equals("")) {
-			 resultList = questionnaireMapper.queryQuestionnaireList(map);
-		}else {
+		List<Map<String, Object>> resultList = new ArrayList<Map<String, Object>>();
+		if (questionnaireName.equals("")) {
+			resultList = questionnaireMapper.queryQuestionnaireList(map);
+		} else {
 			resultList = questionnaireMapper.queryQuestionnaireByName(map);
-		}	
+		}
 		PageInfo<Map<String, Object>> page = new PageInfo<>(resultList);
 		return page;
 	}
@@ -163,23 +185,23 @@ public class QuestionnaireService {
 	public PageInfo showAllDeletedQuestionnaireInfo(Map<String, Object> map) {
 		int pagenum = (int) map.get("pageNum");
 		int pageSize = (int) map.get("pageSize");
-		String questionnaireName = null ;
-		if(map.get("questionnaireName")==null) {
-			questionnaireName="";
-		}else if(map.get("questionnaireName")!=null) {
-			questionnaireName=(String)map.get("questionnaireName");
+		String questionnaireName = null;
+		if (map.get("questionnaireName") == null) {
+			questionnaireName = "";
+		} else if (map.get("questionnaireName") != null) {
+			questionnaireName = (String) map.get("questionnaireName");
 		}
 		PageHelper.startPage(pagenum, pageSize);
-		List<Map<String,Object>> resultList = new ArrayList<Map<String,Object>>();
-		if(questionnaireName.equals("")) {
-			 resultList = questionnaireMapper.queryDeletedQuestionnaireList(map);
-		}else {
+		List<Map<String, Object>> resultList = new ArrayList<Map<String, Object>>();
+		if (questionnaireName.equals("")) {
+			resultList = questionnaireMapper.queryDeletedQuestionnaireList(map);
+		} else {
 			resultList = questionnaireMapper.queryDeletedQuestionnaireByName(map);
-		}	
+		}
 		PageInfo<Map<String, Object>> page = new PageInfo<>(resultList);
 		return page;
 	}
-	
+
 	/**
 	 * 修改问卷信息
 	 * 
@@ -188,60 +210,68 @@ public class QuestionnaireService {
 	 */
 	public int modifyQuestionnaireInfo(Questionnaire questionnaire) {
 		// 设置修改时间
-	
+
 		int result = questionnaireMapper.modifyQuestionnaireInfo(questionnaire);
 		// return questionnaireMapper.updateByPrimaryKeySelective(questionnaire);
 		return result;
 	}
 	
 	/**
-	 *判断是否可以发送并修改信息修改问卷信息
+	 * 修改已发送的问卷为未发送
+	 * 
+	 * @param questionnaire
+	 * @return
+	 */
+	public int modifySendQuestionnaireInfo(Questionnaire questionnaire) {
+		// 设置修改时间
+
+		int result = questionnaireMapper.modifySendQuestionnaireInfo(questionnaire);
+		// return questionnaireMapper.updateByPrimaryKeySelective(questionnaire);
+		return result;
+	}
+
+	/**
+	 * 判断是否可以发送并修改信息修改问卷信息
 	 * 
 	 * @param questionnaire
 	 * @return
 	 */
 	public int modifySendQuestionnaire(Questionnaire questionnaire) {
 		int result = 0;
-		Map<String,Object> target = questionnaireMapper.selectQuestionnaireById(questionnaire.getId());
-		if(target.get("groupname").equals("未发送")) {
+		Map<String, Object> target = questionnaireMapper.selectQuestionnaireById(questionnaire.getId());
+		if (target.get("groupname").equals("未发送")) {
 			// 设置修改时间
-			if(questionnaire.getReleaseTime() == null) {
-				 Date date = DateUtil.getCurrentDate();
-					// questionnaireEntity.setLastUpdateDate(date);
-					questionnaire.setReleaseTime(date);
+			if (questionnaire.getReleaseTime() == null) {
+				Date date = DateUtil.getCurrentDate();
+				// questionnaireEntity.setLastUpdateDate(date);
+				questionnaire.setReleaseTime(date);
 			}
-			
+
 			result = questionnaireMapper.modifyQuestionnaireInfo(questionnaire);
 			// return questionnaireMapper.updateByPrimaryKeySelective(questionnaire);
 			return result;
-		}else {
-			result=0;
+		} else {
+			result = 0;
 		}
-		
+
 		return result;
 	}
-	
-	
-	
 
 	public int deleteQuestionnaireById(String id) throws ParseException {
-        Map<String,Object> m=questionnaireService.queryQuestionnaireById(id);
-    if(!m.get("groupname").equals("未发送")) {
-    	return 1;
-    }
+		Map<String, Object> m = questionnaireService.queryQuestionnaireById(id);
+		if (!m.get("groupname").equals("未发送")) {
+			return 1;
+		}
 
-        questionnaireMapper.deleteQuestionnaireById(id);
-           return 0;
-    }
-	
-	
-	
-	
+		questionnaireMapper.deleteQuestionnaireById(id);
+		return 0;
+	}
+
 	public int deleteQuestionnaireByIdCompletely(String id) throws ParseException {
-    Map<String,Object> m=questionnaireService.queryQuestionnaireById(id);
-    questionnaireMapper.deleteQuestionnaireById(id);
-    return 0;
-   }
+		Map<String, Object> m = questionnaireService.queryQuestionnaireById(id);
+		questionnaireMapper.deleteQuestionnaireById(id);
+		return 0;
+	}
 
 	public int modifyQuestionnaire(Questionnaire questionnaire) {
 
@@ -259,8 +289,6 @@ public class QuestionnaireService {
 		return 1;
 	}
 
-	
-	
 	public int deleteModel(Questionnaire questionnaire) {
 		return questionnaireMapper.deleteModel(questionnaire);
 
